04926ec414f790ed31771f97c339f4ee9fa3ebed
arrell42 <46110420+arrell42@users.noreply.github.com>
2019-03-17 18:26:37 +0700

    1 попытка

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var buttonSearch = document.querySelector(".button-search");
+var searchBlock = document.querySelector(".search-block");
+
+buttonSearch.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  searchBlock.classList.add("search-block-show");
+
+  if (buttonSearch.classList.contains("clicked")) {
+    searchBlock.classList.add("search-block-close");
+  } else {
+    searchBlock.classList.remove("search-block-close");
+  }
+  
+  buttonSearch.classList.toggle("clicked");
+});
+
